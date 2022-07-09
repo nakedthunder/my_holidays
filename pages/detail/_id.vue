32e3b7ccb85d0b90cea0618875378cd655ffaf6c
Jess <nakedthunder00@gmail.com>
2022-07-09 23:29:44 +0900

    add recommand the other pics

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -1,28 +1,52 @@
 //detail > _id.vue
 <template>
-    <div class="container">
-        <div class="main-panel">
-            <img :src="photo.imageUrl" :alt="photo.name" class="product-image">
+    <div class="container_outter">
+        <div class="container">
+            <div class="main-panel">
+                <img :src="photo.imageUrl" :alt="photo.name" class="product-image">
+            </div>
+            <div class="side-panel">
+                <p id="photo_name">{{photo.name}}</p>
+                <p id="photo_price">{{photo.price}}</p>
+                <button class="btnLike" type="button" @click="addToCart">LIKE</button>
+            </div>
         </div>
-    <div class="side-panel">
-        <p id="photo_name">{{photo.name}}</p>
-        <p id="photo_price">{{photo.price}}</p>
-        <button class="btnLike" type="button" @click="addToCart">LIKE</button>
-    </div>
+        <span class="rcom_msg">recommand the other pictures</span>
+        <!-- <recommandImg :rcomPhotos="photos"/> -->
+        <div class="rcom_img_outter">
+            <div class="rcom_img_inner">
+                <ul>
+                    <li v-for="(photo, index) in photos" :key="index" class="rcom_photos">
+                        <img :src="photo.imageUrl" :alt="photo.name" class="rcomImg">
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
-import { fetchProductById, createCartItem } from '@/api/index'
+import { fetchProducts,fetchProductById, createCartItem } from '@/api/index'
 
 export default {
     async asyncData({ params }) {
-        console.log(params.id)
-        const response = await fetchProductById(params.id)
-        console.log("값보이냐", response)
-        const photo = response.data
-        return { photo }
+        const [product, products] = await Promise.all([
+            fetchProductById(params.id),
+            fetchProducts()
+        ])
+        // const product = await fetchProductById(params.id);
+        // const products = await await fetchProducts(); 
+        const photo = product.data;
+        const photosResponse = products.data.map((item) => ({
+            ...item,
+            imageUrl: `${item.imageUrl}?random=${Math.random()}`
+        }))
+        const photos = photosResponse.splice(0, 3)
+        console.log("값보이냐 photo: ", photo);
+        console.log("값보이냐 photos: ", photos);
+
+        return { photo, photos };
     },
     methods: {
         async addToCart() {
@@ -77,5 +101,31 @@ export default {
 }
 button {
     margin-top: 50px;
+}
+.rcomImg {
+    width: 220px;
+    height: 170px;
+    padding-right: 20px;
+}
+
+.rcom_img_inner {
+    display: flex;
+    display: block;
+}
+
+.rcom_img_outter {
+    align-content: center;
+    padding-left: 530px;
+}
+
+.rcom_msg {
+    padding-left: 530px;
+    font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 50px;
+}
+
+.rcom_photos {
+    display: inline-block;
 }
 </style>
